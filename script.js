40fefd23a53042dadad163ced225ed509f1c6789
T10018 <10018@htltraun.at>
2025-07-18 09:06:03 +0200

    try 3.5

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
 document.getElementById("startButton").addEventListener("click", () => {
-  // UI umschalten
   document.getElementById("startscreen").classList.add("hidden");
   document.getElementById("ar-screen").classList.remove("hidden");
 
-  // A-Frame Szene dynamisch erzeugen
-  const arScene = document.createElement("a-scene");
-  arScene.setAttribute("embedded", "");
-  arScene.setAttribute("vr-mode-ui", "enabled: false");
-  arScene.setAttribute("renderer", "logarithmicDepthBuffer: true;");
-  arScene.setAttribute("arjs", "sourceType: webcam; debugUIEnabled: false;");
+  const scene = document.createElement("a-scene");
+  scene.setAttribute("embedded", "");
+  scene.setAttribute("vr-mode-ui", "enabled: false");
+  scene.setAttribute("renderer", "logarithmicDepthBuffer: true;");
+  scene.setAttribute("arjs", "sourceType: webcam; debugUIEnabled: false;");
 
-  arScene.innerHTML = `
+  scene.innerHTML = `
     <a-marker preset="hiro">
       <a-box position="0 0.5 0" material="color: red;"></a-box>
     </a-marker>
     <a-entity camera></a-entity>
   `;
 
-  document.getElementById("ar-screen").appendChild(arScene);
+  document.getElementById("ar-screen").appendChild(scene);
 });
