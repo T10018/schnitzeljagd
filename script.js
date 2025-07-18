54cf443f66b468b5d4146c6586d0c61d994367f1
T10018 <10018@htltraun.at>
2025-07-18 09:12:06 +0200

    try 3.6

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+// Fix für mobile 100vh-Probleme
+function setViewportHeight() {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+setViewportHeight();
+window.addEventListener('resize', setViewportHeight);
+
 document.getElementById("startButton").addEventListener("click", () => {
   document.getElementById("startscreen").classList.add("hidden");
   document.getElementById("ar-screen").classList.remove("hidden");
